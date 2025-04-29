a2da71d51aed10f887002126eb8b422121ce3a15
o.radwan <o.radwan@tammwel.com>
2025-04-29 22:21:30 +0300

    hi

--- src/test/java/Registration_Test.java
+++ src/test/java/Registration_Test.java
@@ -30,9 +30,12 @@ public class Registration_Test extends TestBase {
         public void Valid_Registration_With_NationalID() throws FileNotFoundException, InterruptedException {
             new Registration(driver).Press_On_NationalID().Enter_NationalID("1011111166").Press_On_Title_Login().Press_Continue_Button().Press_On_Mobile_Number_Field();
 new Registration(driver).Enter_Mobile_Number("512345678").Press_On_Title_Registration().Press_On_Join_Now_Button().Press_On_Terms_And_Conditions_Check_Box().Press_Continue_Button();
-new OTP(driver).Press_On_OTP_Field().Enter_OTP();new Registration(driver).Press_Continue_Button().Press_On_Go_To_Nafath_Button().Return_To_App();
+new OTP(driver).Press_On_OTP_Field().Enter_OTP();new Registration(driver).Press_Continue_Button().Press_On_Go_To_Nafath_Button().Return_To_App().Press_On_Set_PIN_Button().Press_Number_One().Press_Number_Two().Press_Number_Three().Press_Number_Four().
+Press_Number_One().Press_Number_Two().Press_Number_Three().Press_Number_Four();
+new Registration(driver).Press_On_Tamawal_Contract_Checkbox_1();
+new Registration(driver).Press_On_Tamawal_Contract_Checkbox_2();
             Assert.assertEquals(
-                    driver.findElements(By.xpath("//android.widget.ScrollView/android.view.View[1]/android.widget.ImageView[2]")).isEmpty(),
+                    driver.findElements(By.xpath("//android.view.View[@content-desc=\"Log in or Sign up\"]")).isEmpty(),
                     false,
                     "No EditText elements found"
             );
