51698d49d0e7f450d0a338e423cf523578ccef30
o.radwan <o.radwan@tammwel.com>
2025-05-13 14:17:56 +0300

    Registration update cases

--- src/test/java/Registration_Test.java
+++ src/test/java/Registration_Test.java
@@ -29,8 +29,8 @@ public class Registration_Test extends TestBase {
 
 @Test
         public void Valid_Registration_With_NationalID_P() throws FileNotFoundException, InterruptedException {
-            new Registration(driver).Press_On_NationalID().Enter_NationalID("1005530090").Press_On_Title_Login().Press_Continue_Button().Press_On_Mobile_Number_Field();
-new Registration(driver).Enter_Mobile_Number("512345678").Press_On_Title_Registration().Press_On_Join_Now_Button().Press_On_Terms_And_Conditions_Check_Box().Press_Continue_Button();
+            new Registration(driver).Press_On_NationalID().Enter_NationalID(Json_Data.Get_json_Data("Registration_Cases_Data","Valid_Registration_With_NationalID_P.NationalID")).Press_On_Title_Login().Press_Continue_Button().Press_On_Mobile_Number_Field();
+new Registration(driver).Enter_Mobile_Number(Json_Data.Get_json_Data("Registration_Cases_Data","Valid_Registration_With_NationalID_P.Mobile_Number")).Press_On_Title_Registration().Press_On_Join_Now_Button().Press_On_Terms_And_Conditions_Check_Box().Press_Continue_Button();
 new OTP(driver).Press_On_OTP_Field().Enter_OTP();new Registration(driver).Press_Continue_Button().Press_On_Go_To_Nafath_Button().Return_To_App().Press_On_Set_PIN_Button().Press_Number_One().Press_Number_Two().Press_Number_Three().Press_Number_Four().
 Press_Number_One().Press_Number_Two().Press_Number_Three().Press_Number_Four();
 new Registration(driver).Press_On_Tamawal_Contract_Checkbox_1().Press_On_Tamawal_Contract_Checkbox_2().Press_Continue_Button();
@@ -43,10 +43,11 @@ new Registration(driver).Press_On_Tamawal_Contract_Checkbox_1().Press_On_Tamawal
         }
 
 
+
     @Test
     public void Registration_With_NationalID_That_Is_Wrong_With_Nafath_N() throws FileNotFoundException, InterruptedException {
-        new Registration(driver).Press_On_NationalID().Enter_NationalID("1011144373").Press_On_Title_Login().Press_Continue_Button().Press_On_Mobile_Number_Field();
-        new Registration(driver).Enter_Mobile_Number("512345678").Press_On_Title_Registration().Press_On_Join_Now_Button().Press_On_Terms_And_Conditions_Check_Box().Press_Continue_Button();
+        new Registration(driver).Press_On_NationalID().Enter_NationalID(Json_Data.Get_json_Data("Registration_Cases_Data","Registration_With_NationalID_That_Is_Wrong_With_Nafath_N.NationalID")).Press_On_Title_Login().Press_Continue_Button().Press_On_Mobile_Number_Field();
+        new Registration(driver).Enter_Mobile_Number(Json_Data.Get_json_Data("Registration_Cases_Data","Registration_With_NationalID_That_Is_Wrong_With_Nafath_N.Mobile_Number")).Press_On_Title_Registration().Press_On_Join_Now_Button().Press_On_Terms_And_Conditions_Check_Box().Press_Continue_Button();
         new OTP(driver).Press_On_OTP_Field().Enter_OTP();new Registration(driver).Press_Continue_Button().Press_On_Go_To_Nafath_Button().Return_To_App();
         Thread.sleep(3000);
         Assert.assertEquals(
@@ -57,8 +58,8 @@ new Registration(driver).Press_On_Tamawal_Contract_Checkbox_1().Press_On_Tamawal
     }
 @Test
     public void Registration_With_Phone_Number_That_Is_Wrong_With_TCC_N() throws FileNotFoundException, InterruptedException {
-        new Registration(driver).Press_On_NationalID().Enter_NationalID("1005530090").Press_On_Title_Login().Press_Continue_Button().Press_On_Mobile_Number_Field();
-        new Registration(driver).Enter_Mobile_Number("511111111").Press_On_Title_Registration().Press_On_Join_Now_Button().Press_On_Terms_And_Conditions_Check_Box().Press_Continue_Button();
+        new Registration(driver).Press_On_NationalID().Enter_NationalID(Json_Data.Get_json_Data("Registration_Cases_Data","Registration_With_Phone_Number_That_Is_Wrong_With_TCC_N.NationalID")).Press_On_Title_Login().Press_Continue_Button().Press_On_Mobile_Number_Field();
+        new Registration(driver).Enter_Mobile_Number(Json_Data.Get_json_Data("Registration_Cases_Data","Registration_With_Phone_Number_That_Is_Wrong_With_TCC_N.Mobile_Number")).Press_On_Title_Registration().Press_On_Join_Now_Button().Press_On_Terms_And_Conditions_Check_Box().Press_Continue_Button();
         new OTP(driver).Press_On_OTP_Field().Enter_OTP();new Registration(driver).Press_Continue_Button().Press_On_Go_To_Nafath_Button().Return_To_App();
         Thread.sleep(3000);
         Assert.assertEquals(
@@ -67,6 +68,7 @@ new Registration(driver).Press_On_Tamawal_Contract_Checkbox_1().Press_On_Tamawal
                 "No EditText elements found"
         );
     }
+
 
 
 
